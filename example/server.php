6431b85f8313cec9b26ed07f8de5b4f78504282d
desarrollosdk2024 <desawil2023@gmail.com>
2024-11-20 13:39:56 -0400

    se agrego la escritura de logs en texto plano

--- example/server.php
+++ example/server.php
@@ -8,4 +8,5 @@ use IntegrationPos\IntegrationPos;
 $integration = new IntegrationPos();
 $integration->initialize('0.0.0.0', 5050, ["device001" => "192.168.1.181","device002"=>"172.20.0.1","device003"=>"192.168.1.1"]);
 $integration->setToken("ANB7xKhiUZmwltVd3f1odcHHM9VAwg02kwmLwtZwHv3SxGCOWLUf5W4G7X22PRj");
+$integration->setupLogging(__DIR__ . '/logs/app.log');
 $integration->start();
